dd42d8b7a0aec8fd4bd936fe41693fc0b1e24aa7
Kana Horie <kana@arcules.com>
2020-10-28 09:01:53 -0700

    Updates

---
+++ precache-manifest.fc0dfcea7dd07d2fe014abb6ef979753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4026b158de2cf1733acd30c83bc72bee",
+    "url": "/udemy-react-newsapp/index.html"
+  },
+  {
+    "revision": "d93e6dd6b05b8d8e08bb",
+    "url": "/udemy-react-newsapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e6bb6ee9d4a6bae28ae2",
+    "url": "/udemy-react-newsapp/static/css/main.bfada26c.chunk.css"
+  },
+  {
+    "revision": "d93e6dd6b05b8d8e08bb",
+    "url": "/udemy-react-newsapp/static/js/2.3f852ef3.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/udemy-react-newsapp/static/js/2.3f852ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6bb6ee9d4a6bae28ae2",
+    "url": "/udemy-react-newsapp/static/js/main.35b898d2.chunk.js"
+  },
+  {
+    "revision": "7e781ff1e4c96dc67aed",
+    "url": "/udemy-react-newsapp/static/js/runtime-main.e7f3359e.js"
+  }
+]);
